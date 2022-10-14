417a048b7230d8332b710398716dba44a351d5bb
zaddywealthtech <kanayovictorjr@gmail.com>
2022-10-14 12:39:46 -0700

    end coding

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,11 +2,11 @@
 
 /**
  *
- *  * Description: main - Prints out a string to stdout.
+ *   Description: main - Prints out a string to stdout.
  *
- *   * Return: 0 if success.
+ *    Return: 0 if success.
  *
- *    */
+ */
 
 int main(void)
 
