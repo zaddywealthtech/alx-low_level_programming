a8582a99c1c85818c230b086685da1d094a82909
zaddywealthtech <kanayovictorjr@gmail.com>
2022-10-14 13:02:05 -0700

    end coding

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,21 +1,10 @@
 #include <stdio.h>
-
 /**
- *
- *   main-entry point for the program.
- *
- *   
- *
- *     Return:0 if no errors, return non zero if errors
- *
+ * Description: main - Prints out a string to stdout.
+ * Return: 0 if success.
  */
-
 int main(void)
-
 {
-
-	 printf("with proper grammar, but the outcome is a piece of art,\n");
-
-	  return (0);
-
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
