48501d37265e3f932e6ad9065a9850750d079bd4
zaddywealthtech <kanayovictorjr@gmail.com>
2022-10-14 10:14:53 -0700

    end coding

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,19 @@
 #include <stdio.h>
+
 /**
- *  *main- Serves as the entry point for the program
- *   *
- *    *Return: By default returns zero if no erros otherwise returns non-zero value.
- *     */
+ *
+ *  * Description: main - Prints out a string to stdout.
+ *
+ *   * Return: 0 if success.
+ *
+ *    */
+
 int main(void)
+
 {
-		puts("\"Programming is like building a multilingual puzzle");
-			return (0);
+
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+
 }
