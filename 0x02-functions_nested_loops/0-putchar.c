ea368be6625d6eb7ab9f07f7ee6c3a88c78cbdfd
zaddywealthtech <kanayovictorjr@gmail.com>
2022-10-19 14:59:40 -0700

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,20 @@
+#include <stdio.h>
 #include "main.h"
-
 /**
- *  main - print _putchar
- *  is written by kanayo
- *  Return: 0
- */
+*main - Begin here
+*
+*Return: Value of 0 success
+*/
 
 int main(void)
 {
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
-return (0);
+	char text[] = "_putchar";
+	int i;
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(text[i]);
+	}
+	_putchar('\n');
+	return (0);
 }
